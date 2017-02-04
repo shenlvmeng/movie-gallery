03c810dfa3b41334c036b58585c6eeef6165b3aa
Zinc <xin_yuanming@163.com>
2017-02-05 00:39:07 +0800

    add navbar and picture wall with vue

---
+++ assets/base.js
@@ -0,0 +1,171 @@
+var res, tag_list = {};
+
+//asynchronical XMLHttpRequest
+function loadFile (url, callback) {
+	var req = new XMLHttpRequest();
+	req.onreadystatechange = function () {
+		if (req.readyState == 4) {
+			if (req.status == 200) {
+				callback.apply(req);
+			} else {
+				console.log("Error", req.statusText);
+			}
+		}
+	};
+	req.open("GET", url, true);
+	req.send(null);
+}
+
+loadFile("./gallery_info.json", function(){
+	res = JSON.parse(this.responseText);
+	//print meta data
+	console.log(res.name + ": " + res.description);
+	console.log("Author: " + res.author);
+	//export tags information
+	var len = res.content.length;
+	for (var i = 0; i < len; i++) {
+		//read from res.content[i].date
+		var year = Math.floor(res.content[i].date / 10000);
+		if (year <= 2012) {
+			year = "~2012";
+		}
+		year += "";
+		if (!tag_list[year]) {
+			tag_list[year] = [i];
+		} else {
+			tag_list[year].push(i);
+		}
+		//read from res.content[i].tags
+		res.content[i].tags.forEach(function (val) {
+			if (!tag_list[val]) {
+				tag_list[val] = [i];
+			} else {
+				tag_list[val].push(i);
+			}
+		});
+	}
+	//console.log(tag_list);
+
+
+	var Wall = {
+		template: '<div id="photos">\
+			<figure v-for="item in items"><img :src="item"></figure>\
+		</div>',
+		props: ['factors'],
+		computed: {
+			items: function () {
+				var tmparr = [],
+	            	tmp = {},
+	            	myarr = Array.apply(null, Array(res.content.length)).map(function (x, i) { return i; });
+            	this.factors.forEach(function(val) {
+            		myarr = _.intersection(tag_list[val], myarr);
+            		if ([] == myarr) {
+            			return;
+            		}
+            		//console.log(myarr);
+            	});
+
+	            for (var i = 0, len = myarr.length; i < len; i++) {
+	                if (tmp[myarr[i]] == undefined) {
+	                    tmp[myarr[i]] = 1
+	                    tmparr.push("./assets/img/" + myarr[i] + "." + res.content[myarr[i]].type);
+	                }
+	            }
+	            return tmparr;
+			}
+		}
+	}
+
+	var vm = new Vue({
+		data: {
+			filter: ""
+		},
+		mounted: function () {
+			document.querySelectorAll(".list-item").forEach(function (val) {
+				val.addEventListener("click", function (e){
+					document.querySelectorAll(".list-item").forEach(function (val) {
+						val.children[1].style.display = "";
+					});
+					if (this.children[1].style.display == "") {
+						this.children[1].style.display = "block";
+					} else {
+						this.children[1].style.display = "";
+					}
+
+					if (!e){
+						var e = window.event;
+					}
+					e.cancelBubble = true;
+					if (e.stopPropagation){
+						e.stopPropagation();
+					}
+				});
+				val.addEventListener("touchend", function (e) {
+					document.querySelectorAll(".list-item").forEach(function (val) {
+						val.children[1].style.display = "";
+					});
+					if (this.children[1].style.display == "") {
+						this.children[1].style.display = "block";
+					} else {
+						this.children[1].style.display = "";
+					}
+					if (!e){
+						var e = window.event;
+					}
+					e.cancelBubble = true;
+					if (e.stopPropagation){
+						e.stopPropagation();
+					}
+				});
+			});
+			window.addEventListener("click", function () {
+				document.querySelectorAll(".list-item").forEach(function (val) {
+					val.children[1].style.display = "";
+				});
+			});
+			window.addEventListener("touchend", function () {
+				document.querySelectorAll(".list-item").forEach(function (val) {
+					val.children[1].style.display = "";
+				});
+			});
+		},
+		methods: {
+			addPreface: function () {
+				if (this.filter == "") {
+					this.filter = "preface";
+				}
+				if (this.factors.indexOf("preface") == -1) {
+					this.filter += ",preface";
+				}
+			},
+			addShot: function () {
+				if (this.filter == "") {
+					this.filter = "screenshot";
+				}
+				if (this.factors.indexOf("screenshot") == -1) {
+					this.filter += ",screenshot";
+				}
+			},
+			addTag: function (event) {
+				var tag = event.target.innerHTML;
+				if (this.filter == "") {
+					this.filter = tag;
+				}
+				if (this.factors.indexOf(tag) == -1) {
+					this.filter += ("," + tag);
+				}
+			}
+		},
+		computed: {
+			factors: function () {
+				return this.filter.split(",", 10).filter(function(element){
+					return element != "";
+				});
+			}
+		},
+		components: {
+			picwall: Wall
+		}
+	}).$mount("#app");
+
+});
